c36ce2f9633365996c6cc9284f8696b8995ddcf1
rakib8680 <rakib.cst.1st@gmail.com>
2024-07-17 19:31:50 +0600

    User Model Created

---
+++ prisma/migrations/20240717133105_init_user_model/migration.sql
@@ -0,0 +1,27 @@
+-- CreateEnum
+CREATE TYPE "USER_ROLE" AS ENUM ('USER', 'ADMIN', 'INSTRUCTOR');
+
+-- CreateEnum
+CREATE TYPE "USER_STATUS" AS ENUM ('ACTIVE', 'BLOCKED');
+
+-- CreateEnum
+CREATE TYPE "COURSE_TYPE" AS ENUM ('FREE', 'PAID');
+
+-- CreateEnum
+CREATE TYPE "COURSE_STATUS" AS ENUM ('PUBLISHED', 'DRAFT', 'DELETED');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "USER_ROLE" NOT NULL DEFAULT 'USER',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ const app: Application = express();
 
 // parsers
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 
 
 
